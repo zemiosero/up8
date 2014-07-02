bdc9079fab133ae26eca1617a2fc66221a1600ea
student <student@PC634S07.fgakusei.local>
2014-07-02 17:23:52 +0900

    2

--- gen/com/example/gamesample/R.java
+++ gen/com/example/gamesample/R.java
@@ -27,43 +27,46 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
         public static final int ishikabe1=0x7f020003;
         public static final int pre_to1=0x7f020004;
+        public static final int preto2=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001b;
+        public static final int action_settings=0x7f08001c;
         public static final int btn1=0x7f080004;
         public static final int btn2=0x7f080003;
         public static final int btn3=0x7f080002;
-        public static final int btn_back=0x7f080019;
-        public static final int btn_flag=0x7f08001a;
-        public static final int button2=0x7f080005;
-        public static final int button3=0x7f080007;
+        public static final int btn_back=0x7f08001a;
+        public static final int btn_flag=0x7f08001b;
+        public static final int button2=0x7f080006;
+        public static final int button3=0x7f080008;
         public static final int container=0x7f080000;
-        public static final int imageSwitcher1=0x7f080006;
-        public static final int sbt1=0x7f080009;
-        public static final int sbt10=0x7f080012;
-        public static final int sbt11=0x7f080013;
-        public static final int sbt12=0x7f080014;
-        public static final int sbt13=0x7f080015;
-        public static final int sbt14=0x7f080016;
-        public static final int sbt15=0x7f080017;
-        public static final int sbt16=0x7f080018;
-        public static final int sbt2=0x7f08000a;
-        public static final int sbt3=0x7f08000b;
-        public static final int sbt4=0x7f08000c;
-        public static final int sbt5=0x7f08000d;
-        public static final int sbt6=0x7f08000e;
-        public static final int sbt7=0x7f08000f;
-        public static final int sbt8=0x7f080010;
-        public static final int sbt9=0x7f080011;
-        public static final int textView1=0x7f080008;
+        public static final int imageSwitcher1=0x7f080007;
+        public static final int imageView1=0x7f080005;
+        public static final int sbt1=0x7f08000a;
+        public static final int sbt10=0x7f080013;
+        public static final int sbt11=0x7f080014;
+        public static final int sbt12=0x7f080015;
+        public static final int sbt13=0x7f080016;
+        public static final int sbt14=0x7f080017;
+        public static final int sbt15=0x7f080018;
+        public static final int sbt16=0x7f080019;
+        public static final int sbt2=0x7f08000b;
+        public static final int sbt3=0x7f08000c;
+        public static final int sbt4=0x7f08000d;
+        public static final int sbt5=0x7f08000e;
+        public static final int sbt6=0x7f08000f;
+        public static final int sbt7=0x7f080010;
+        public static final int sbt8=0x7f080011;
+        public static final int sbt9=0x7f080012;
+        public static final int textView1=0x7f080009;
         public static final int title=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int floar1_1=0x7f030001;
-        public static final int fragment_main=0x7f030002;
-        public static final int select=0x7f030003;
-        public static final int test1=0x7f030004;
+        public static final int floar1_2=0x7f030002;
+        public static final int fragment_main=0x7f030003;
+        public static final int select=0x7f030004;
+        public static final int test1=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/gamesample/Room1Activity.java
+++ src/com/example/gamesample/Room1Activity.java
@@ -1,7 +1,13 @@
 package com.example.gamesample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
 
 public class Room1Activity extends Activity {
 
@@ -10,8 +16,15 @@ public class Room1Activity extends Activity {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.floar1_1);
+
+		Button nextBtn = (Button)findViewById(R.id.btnR1);
+        nextBtn.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				Intent it =new Intent();
+				it.setClass(Room1Activity.this, Room2Activity.class);
+				startActivity(it);
+
+			}});
 	}
-
-
 
 }
